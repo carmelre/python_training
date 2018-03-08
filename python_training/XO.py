ec27ea250aedd6a047ab183b66639cf842e0a3ef
carmel <carmel.r@sygnia.co>
2018-03-08 14:13:22 +0200

    now its a bit better :)

--- python_training/XO.py
+++ python_training/XO.py
@@ -1,87 +1,93 @@
 import random
 
 
-class XoGame:
+class XOGame:
     """
-    Creates a XO game for two players, in any wanted board size (n>2)
+    Creates a XO game for two players, in any wanted board size (n>2).
     """
 
-    def __init__(self, players, board_size):
+    def __init__(self, player1, player2, board_size):
         """
-        :param players: A list with the player names.
+        Initiates the class and prepares all the necessary data.
+
+        :param player1: The name of the first player
+        :param player2: The name of the second player
         :param board_size: The length of the boards rows and columns.
         """
-        self.players_symbols = ["X", "O"]
-        # Create a mapping between the player symbol and name
-        self.players = {symbol_and_player[0]: symbol_and_player[1] for symbol_and_player in
-                        list(zip(self.players_symbols, players))}
+        # Create a mapping between the player representation and name
+        self.players = {0: player1, 1: player2}
+        # Create mapping between player representation and printable symbol
+        self.symbols_mapping = {0: "X", 1: "O", None: " "}
         self.board_size = board_size
-        # I Use range so each row would create a different list in the memory
-        self.board = [[" "] * board_size for row in range(self.board_size)]
-        self.taken_turns = 0
-        self.current_player = random.choice(self.players_symbols)
+        # Range is used so each row would create a different list in the memory
+        self.board = [[None] * board_size for row in range(self.board_size)]
+        self.turns_counter = 0
+        self.current_player = random.choice(list(self.players.keys()))
 
+    def start_game(self):
+        """
+        Calls for turns to be taken until the game is over.
+        """
+        self.print_board()
         while not self.has_game_ended():
             self.take_turn()
+            self.print_board()
 
     def print_board(self):
         """
-        Prints the current board of the game
+        Prints the current board of the game.
         """
-        first_line = '   ' + ' '.join([str(num) for num in range(self.board_size)]) + '\n'
-        board_lines = [str(line_index) + ' ' + '|' + '{}|' * self.board_size + '\n' for line_index in
-                       range(self.board_size)]
-        final_string = first_line + ('  ' + '-' * (3 * self.board_size - (self.board_size - 1)) + '\n').join(
-            board_lines)
-        print(final_string.format(*[slot for row in self.board for slot in row]))
+        # Prints the first line that looks like this (for n = 3):
+        #   0 1 2
+        print('   ' + ' '.join([str(num) for num in range(self.board_size)]))
+        # Creates a line that separates the board rows (calculated according to the number of slots)
+        # Eventually the line would look like this -------------
+        separator_line = '  ' + '-' * (3 * self.board_size - (self.board_size - 1))
+        for row_number in range(self.board_size):
+            # Create a line template in which printable values will be formatted.
+            line_template = (str(row_number) + ' |' + '{}|' * self.board_size)
+            # Switch the values in a row to printable ones according to our mapping.
+            printable_values = [self.symbols_mapping[internal_value] for internal_value in self.board[row_number]]
+            # Format the template with the printable values created above.
+            print(line_template.format(*printable_values))
+            if row_number < self.board_size - 1:
+                print(separator_line)
 
     def take_turn(self):
         """
-        this function performes several actions:
-        - switch the player that has the next turn
-        - receive a slot from the player and check if it available
-        - positions the players symbol on the board
+        Performs several actions:
+        - Switch the player that has the next turn
+        - Receive a slot from the player and check if it available
+        - Positions the players symbol on the board
         """""
-        self.print_board()
 
-        if not self.taken_turns == 0:
-            self.current_player = self.players_symbols[(self.players_symbols.index(self.current_player) + 1) % 2]
+        if self.turns_counter > 0:
+            self.current_player = (self.current_player + 1) % 2
 
-        is_slot_available = False
-        while not is_slot_available:
-            print('OK {}, Its your turn!'.format(self.players[self.current_player]))
-            row_index = int(input("Please enter row number >"))
-            column_index = int(input("Please enter column number >"))
-            if row_index not in range(self.board_size) or column_index not in range(self.board_size):
-                print("You have entered invalid slot numbers, please try again.")
-                continue
-            elif self.board[row_index][column_index] == " ":
-                is_slot_available = True
-                self.board[row_index][column_index] = self.current_player
-            else:
-                print("That slot is not available, please choose again.")
+        print('OK {}, Its your turn!'.format(self.players[self.current_player]))
 
-        self.taken_turns += 1
+        row_index, column_index = self._get_validated_slot_number_input()
+        self.board[row_index][column_index] = self.current_player
+        self.turns_counter += 1
 
-    def has_game_ended(self):
+    def has_game_ended(self) -> bool:
         """
-        Check whether a player has one/ the game was tied
-        :return: True/False
+        Check whether a player has won/the game was tied.
         """
 
-        if self.taken_turns < (self.board_size * 2 - 1):
+        if self.turns_counter < (self.board_size * 2 - 1):
             return False
 
-        if self.taken_turns == self.board_size ** 2:
+        if self.turns_counter == self.board_size ** 2:
             print("The game has ended with a tie!")
             return True
 
         if any([self._check_for_winning_sequence(row) for row in self.board]):
             return True
 
-        for column_number in range(self.board_size):
-            if self._check_for_winning_sequence([row[column_number] for row in self.board]):
-                return True
+        columns_list = [[row[column_number] for row in self.board] for column_number in range(self.board_size)]
+        if any([self._check_for_winning_sequence(column) for column in columns_list]):
+            return True
 
         left_to_right_diagonal = [self.board[index][index] for index in range(self.board_size)]
         if self._check_for_winning_sequence(left_to_right_diagonal):
@@ -91,28 +97,73 @@ class XoGame:
         if self._check_for_winning_sequence(right_to_left_diagonal):
             return True
 
-    def _check_for_winning_sequence(self, sequence):
+    def _check_for_winning_sequence(self, sequence) -> bool:
         """
         Check if a specific sequence is comprised of a uniform symbol, and indicates a win.
-        :param sequence:
-        :return: True/False
+
+        :param sequence: The sequence that should be checked.
         """
         if sequence.count(self.current_player) == self.board_size:
-            self.print_board()
             print('The winner is {}'.format(self.players[self.current_player]))
             return True
 
+    def _get_validated_slot_number_input(self):
+        """
+        Get a valid slot number input from the user.
+
+        :return: a tuple with two items: (row index, column index)
+        """
+        is_slot_number_valid = False
+
+        while not is_slot_number_valid:
+            row_index = input("Please enter row number >")
+            column_index = input("Please enter column number >")
+            if not row_index.isnumeric() or not column_index.isnumeric():
+                print("Slot locations must be numbers, please try again")
+                continue
+            row_index = int(row_index)
+            column_index = int(column_index)
+            if row_index not in range(self.board_size) or column_index not in range(self.board_size):
+                print("Make sure the slot numbers you have entered are within the range of the board.")
+                continue
+            if self.board[row_index][column_index]:
+                print("That slot is already taken, choose again")
+                continue
+            else:
+                is_slot_number_valid = True
+
+        return row_index, column_index
+
+
+def get_validated_board_size() -> int:
+    """
+    Validate that the user gives a numeric board size that is bigger than 2.
+
+    :return: a board size after validation
+    """
+    is_board_size_valid = False
+    while not is_board_size_valid:
+        board_size_input = input('Please enter board size')
+        if not board_size_input.isnumeric():
+            print("invalid input, please enter a number")
+            continue
+        board_size_input = int(board_size_input)
+        if board_size_input < 3:
+            print('board size must be bigger than 3')
+            continue
+        else:
+            is_board_size_valid = True
+
+    return board_size_input
+
 
 def main():
-    player1 = str(input('Please enter the name of player one >'))
-    player2 = str(input('Please enter the name of player two >'))
-    players = [player1, player2]
-    board_size = int(input('Please enter board size'))
-    while board_size < 3:
-        print('board size must be bigger than 3')
-        board_size = input('Please enter board size')
+    player1 = input('Please enter the name of player one >')
+    player2 = input('Please enter the name of player two >')
+    board_size = get_validated_board_size()
 
-    XoGame(players, board_size)
+    game = XOGame(player1, player2, board_size)
+    game.start_game()
 
 
 if __name__ == "__main__":
