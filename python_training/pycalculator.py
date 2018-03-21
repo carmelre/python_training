826aaf05dd5847d86e32a3f6435fade978c6af1a
carmel <carmel.r@sygnia.co>
2018-03-21 10:53:29 +0200

    added test and functionality

--- python_training/pycalculator.py
+++ python_training/pycalculator.py
@@ -1,16 +1,35 @@
 
+
 class Calculator:
     def __init__(self):
-        pass
+        self.saved_num = None
 
-    @staticmethod
-    def add(a, b):
+    def add(self, a, b):
+        self.check_input(a, b)
         return a + b
 
-    @staticmethod
-    def substract(a, b):
+    def subtract(self, a, b):
+        self.check_input(a, b)
         return a - b
 
-    @staticmethod
-    def divide(a, b):
+    def divide(self, a, b):
+        self.check_input(a, b)
         return a / b
+
+    def multiply(self, a, b):
+        self.check_input(a, b)
+        return a * b
+
+    def get_saved_number(self):
+        return self.saved_num
+
+    def set_saved_num(self, number):
+        self.check_input(number)
+        self.saved_num = number
+
+    @staticmethod
+    def check_input(*args):
+        if any([arg for arg in args if type(arg) is not int]):
+                raise TypeError
+
+
--- python_training/test_me.py
+++ python_training/test_me.py
@@ -2,7 +2,7 @@ import pytest
 from .pycalculator import Calculator
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def calculator():
     return Calculator()
 
@@ -11,15 +11,38 @@ def test_add_a_to_b(calculator):
     assert calculator.add(2, 3) == 5
 
 
-def test_substract_b_from_a(calculator):
-    assert calculator.substract(3, 2) == 1
+def test_add_string_to_string_raises(calculator):
+    with pytest.raises(Exception):
+        assert calculator.add("a", "b")
 
 
-def test_devide_a_to_b(calculator):
-    assert calculator.divide(4,2) == 2
+def test_subtract_b_from_a(calculator):
+    assert calculator.subtract(3, 2) == 1
 
 
-def test_devide_num_to_0_fils(calculator):
+def test_divide_a_to_b(calculator):
+    assert calculator.divide(4, 2) == 2
+
+
+def test_divide_num_to_0_raises(calculator):
     with pytest.raises(ZeroDivisionError):
         calculator.divide(2, 0)
 
+
+def test_multiply_a_by_b(calculator):
+    assert calculator.multiply(2, 3) == 6
+
+
+def test_save_number(calculator):
+    calculator.set_saved_num(3)
+    assert calculator.get_saved_number() == 3
+
+
+def test_save_noninteger_raises(calculator):
+    with pytest.raises(Exception):
+        calculator.set_saved_num("a")
+
+
+def test_use_unset_saved_number_raises(calculator):
+    with pytest.raises(Exception):
+        calculator.add(calculator.get_saved_number(), 3)
