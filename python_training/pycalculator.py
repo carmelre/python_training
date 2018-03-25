585eeee4a15b46b1e986e0ec8dc24eee93906bee
carmel <carmel.r@sygnia.co>
2018-03-25 12:00:59 +0300

    fixed according to comments

--- python_training/pycalculator.py
+++ python_training/pycalculator.py
@@ -1,35 +1,36 @@
+def check_input(original_func):
+    def wrapper(*args):
+        if any([True for arg in args[1:] if not (type(arg) is int or type(arg) is float)]):
+            raise TypeError("The given parameters are not all integers")
+        else:
+            return original_func(*args)
+    return wrapper
 
 
 class Calculator:
     def __init__(self):
         self.saved_num = None
 
+    @check_input
     def add(self, a, b):
-        self.check_input(a, b)
         return a + b
 
+    @check_input
     def subtract(self, a, b):
-        self.check_input(a, b)
         return a - b
 
+    @check_input
     def divide(self, a, b):
-        self.check_input(a, b)
         return a / b
 
+    @check_input
     def multiply(self, a, b):
-        self.check_input(a, b)
         return a * b
 
     def get_saved_number(self):
         return self.saved_num
 
+    @check_input
     def set_saved_num(self, number):
-        self.check_input(number)
+        print(number)
         self.saved_num = number
-
-    @staticmethod
-    def check_input(*args):
-        if any([arg for arg in args if type(arg) is not int]):
-                raise TypeError
-
-
--- python_training/test_me.py
+++ python_training/test_me.py
@@ -1,36 +1,62 @@
 import pytest
 from .pycalculator import Calculator
 
+invalid_parameters = [('a', 'b'), (1, 'a'), (1, None), ({'a': 1}, 2)]
+
 
 @pytest.fixture()
 def calculator():
     return Calculator()
 
 
-def test_add_a_to_b(calculator):
-    assert calculator.add(2, 3) == 5
+@pytest.fixture(params=[
+    Calculator().add,
+    Calculator().subtract,
+    Calculator().multiply,
+    Calculator().divide
+
+])
+def calculator_math_methods(request):
+    return request.param
 
 
-def test_add_string_to_string_raises(calculator):
-    with pytest.raises(Exception):
-        assert calculator.add("a", "b")
+@pytest.mark.parametrize('x, y', invalid_parameters)
+def test_math_methods_invalid_input_raises(x, y, calculator_math_methods):
+    with pytest.raises(TypeError):
+        assert calculator_math_methods(x, y)
 
 
-def test_subtract_b_from_a(calculator):
-    assert calculator.subtract(3, 2) == 1
+@pytest.mark.parametrize('x, y, expected', [
+    (1, 2, 3),
+    (2.2, 1.1, 3.3)])
+def test_add_valid_input(x, y, expected, calculator):
+    assert round(calculator.add(x, y), 2) == expected
 
 
-def test_divide_a_to_b(calculator):
-    assert calculator.divide(4, 2) == 2
+@pytest.mark.parametrize('x, y, expected', [
+    (2, 1, 1),
+    (2.2, 1.1, 1.1)])
+def test_subtract_valid_input(x, y, expected, calculator):
+    assert round(calculator.subtract(x, y), 2) == expected
 
 
-def test_divide_num_to_0_raises(calculator):
+@pytest.mark.parametrize('x, y, expected', [
+    (4, 2, 2),
+    (4.4, 2.2, 2.0)])
+def test_divide_valid_input(x, y, expected, calculator):
+    assert calculator.divide(x, y) == expected
+
+
+@pytest.mark.parametrize('x, y, expected', [
+    (2, 2, 4),
+    (1.0, 2.0, 2.0)])
+def test_multiply_vlaid_input(x, y, expected, calculator):
+    assert calculator.multiply(x, y) == expected
+
+
+def test_divide_num_by_0_raises(calculator):
     with pytest.raises(ZeroDivisionError):
         calculator.divide(2, 0)
-
-
-def test_multiply_a_by_b(calculator):
-    assert calculator.multiply(2, 3) == 6
 
 
 def test_save_number(calculator):
@@ -38,11 +64,16 @@ def test_save_number(calculator):
     assert calculator.get_saved_number() == 3
 
 
-def test_save_noninteger_raises(calculator):
-    with pytest.raises(Exception):
-        calculator.set_saved_num("a")
+@pytest.mark.parametrize('invalid_value', [
+    "a",
+    None,
+    {'a': 1}
+])
+def test_save_invalid_value_raises(invalid_value, calculator):
+    with pytest.raises(TypeError):
+        calculator.set_saved_num(invalid_value)
 
 
 def test_use_unset_saved_number_raises(calculator):
-    with pytest.raises(Exception):
+    with pytest.raises(TypeError):
         calculator.add(calculator.get_saved_number(), 3)
