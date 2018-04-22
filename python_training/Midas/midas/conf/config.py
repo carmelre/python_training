5deace4636e10616dc2dfc1960153c691fcfe884
carmel <carmel.r@sygnia.co>
2018-04-22 09:30:17 +0300

    moved mock data to a dedicated file

--- python_training/Midas/midas/conf/config.py
+++ python_training/Midas/midas/conf/config.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import pkg_resources
 
 
@@ -6,10 +5,3 @@ DB_FILE = pkg_resources.resource_filename('midas', 'db/midas.db')
 
 OPERATIONAL_DB = f'sqlite:///{DB_FILE}'
 
-MEMBERS_INFO = [{'first_name': 'carmel', 'last_name': 'reubinoff', 'role': 'dev', 'location': 'Tozeret Haaretz'},
-                {'first_name': 'Anya', 'last_name': 'tch', 'role': 'dev', 'location': 'Zikim'},
-                {'first_name': 'Alon', 'last_name': 'Shenkler', 'role': 'dev', 'location': 'Tozeret Haaretz'}]
-ORGANIZATIONS_INFO = [{'name': 'Sygnia', 'prime_location': 'Tozeret Haaretz'},
-                      {'name': 'Claroty', 'prime_location': 'Tozeret Haaretz'}]
-EVENTS_INFO = [{'date': datetime.now(), 'location': 'Mizpe Hayamim'},
-               {'date': datetime.now(), 'location': 'Zappa Herzliya'}]
