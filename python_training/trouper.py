d9eaed29ef31f11666d005a107991219ff5b497f
carmel <carmel.r@sygnia.co>
2018-04-22 13:53:39 +0300

    Documentation

--- python_training/trouper.py
+++ python_training/trouper.py
@@ -1,6 +1,13 @@
 
 class Number(int):
+    """
+    An object that extends the int object and adds functionality over the dividers of a number.
+    """
     def __init__(self, number):
+        """
+        Instantiates a number object and prepares the dividers list.
+        :param number:
+        """
         super().__init__()
         self.number = number
         self.dividers = [1, number]
@@ -8,7 +15,7 @@ class Number(int):
 
     def _get_all_dividers(self):
         """
-        Appends to the dividers list the rest of the numbers dividers(that are not 1 or the number itself)
+        Appends to the dividers list the rest of the numbers dividers(that are not 1 or the number itself).
         """
         square_root = self.number**0.5
         for i in range(2, int(square_root) + 1):
