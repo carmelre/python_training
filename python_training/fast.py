48fd6a54ae20ffe4579b3a54648de9cda05fb5f9
carmel <carmel.r@sygnia.co>
2018-04-23 20:29:25 +0300

    wrote a basic generator

--- python_training/fast.py
+++ python_training/fast.py
@@ -1,14 +1,25 @@
 from functools import partial
 import time
-import timeit
 
 
 def timer(func):
-    runtime_avg = None
+    """
+    A generator that executes a function and yields a moving average of the function runtime.
+    :param func: The function that would be tested.
+    :return: Average runtime.
+    """
+    runtime_stats = {'sum_of_run_times': 0, 'number_of_executions': 0}
+    rounds = None
     while True:
-        # define the number of runs
-
-
+        if rounds is None:
+            rounds = 1
+        for cycle in range(rounds):
+            start_time = time.monotonic()
+            func()
+            elapsed_time = time.monotonic() - start_time
+            runtime_stats['sum_of_run_times'] += elapsed_time
+        runtime_stats['number_of_executions'] += rounds
+        rounds = yield runtime_stats['sum_of_run_times'] / runtime_stats['number_of_executions']
 
 
 def super_slow_func(sleep_time):
@@ -17,4 +28,9 @@ def super_slow_func(sleep_time):
 
 if __name__ == '__main__':
     slow_func_with_arguments = partial(super_slow_func, sleep_time=1)
-    timeit.timeit()
+    g = timer(slow_func_with_arguments)
+    print(next(g))
+    g.send(2)
+    print(next(g))
+    print(next(g))
+
