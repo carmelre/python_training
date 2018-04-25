f70dd4f4a9683fb02b8ecf1639f2232eabccd49b
carmel <carmel.r@sygnia.co>
2018-04-25 19:22:45 +0300

    completed the three functions that were required

--- python_training/implement_me.py
+++ python_training/implement_me.py
@@ -1,5 +1,6 @@
 import datetime as dt
 from typing import List
+from netaddr import valid_ipv4
 
 
 def index(data: List, redis_connection):
@@ -21,23 +22,31 @@ def index(data: List, redis_connection):
         redis_connection.zadd(ip, *values)
 
 
-def get_device_histogram(ip, n, redis_connection):
+def get_device_histogram(ip: str, number_of_results: int, redis_connection):
     """
     Return the latest 'n' entries for the given 'ip'.
+
+    :param ip: The ip of which results would be returned.
+    :param number_of_results: The number of result that should be returned.
+    :param redis_connection: An instantiated redis.Redis object.
+
     """
-    raw_results = redis_connection.zrange(ip, -n, -1)
+    raw_results = redis_connection.zrange(ip, -number_of_results, -1)
     formatted_results = [result.decode().split(',') for result in raw_results]
     dict_splitted_results = [
         {'timestamp': dt.datetime.fromtimestamp(int(result[0])), 'protocol': result[1]} for result in formatted_results
-    ].reverse()
+    ]
+    dict_splitted_results.reverse()
     return dict_splitted_results
 
 
-def get_devices_status():
+def get_devices_status(redis_connection):
     """
     Return a list of every ip and the latest time it was seen it.
+
+    :param redis_connection: An instantiated redis.Redis object.
     """
-    return [
-        ('4.2.2.4', dt.datetime.now()),
-        ('8.8.8.8', dt.datetime.now())
-    ]
+    ips = redis_connection.keys()
+    return [(ip, dt.datetime.fromtimestamp(int(redis_connection.zrange(ip, -1, -1)[0].decode().split(',')[0])))
+            for ip in ips if valid_ipv4(ip.decode())]
+
